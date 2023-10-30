43e28eb06d4a260371260363f470419dde915d07
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2023-10-30 20:55:27 +0000

    Complete day 6

---
+++ src/06/day06.ts
@@ -0,0 +1,65 @@
+import fs from "fs"
+
+const part1 = () => {
+  const lines = readLines()
+
+  const len = lines[0]!.length
+
+  const message: string[] = []
+
+  for (let i = 0; i < len; i++) {
+    const letterCounts: Record<string, number> = {}
+    let maxLetter = ""
+
+    for (const line of lines) {
+      const letter = line[i]!
+      letterCounts[letter] = (letterCounts[letter] || 0) + 1
+
+      if (!maxLetter || letterCounts[letter]! > letterCounts[maxLetter]!) {
+        maxLetter = letter
+      }
+    }
+
+    message.push(maxLetter)
+  }
+
+  console.log(message.join(""))
+}
+
+const part2 = () => {
+  const lines = readLines()
+
+  const len = lines[0]!.length
+
+  const message: string[] = []
+
+  for (let i = 0; i < len; i++) {
+    const letterCounts: Record<string, number> = {}
+
+    for (const line of lines) {
+      const letter = line[i]!
+      letterCounts[letter] = (letterCounts[letter] || 0) + 1
+    }
+
+    let minLetter = ""
+    let minCount = Infinity
+    for (const letter in letterCounts) {
+      const letterCount = letterCounts[letter]!
+      if (letterCount < minCount) {
+        minLetter = letter
+        minCount = letterCount
+      }
+    }
+
+    message.push(minLetter)
+  }
+
+  console.log(message.join(""))
+}
+
+const readLines = () => {
+  const filepath = new URL("./index.txt", import.meta.url).pathname
+  return fs.readFileSync(filepath, "utf-8").toString().split("\n")
+}
+
+export const day06 = { part1, part2 }
--- src/run.ts
+++ src/run.ts
@@ -3,6 +3,7 @@ import { day02 } from "./02/day02.js"
 import { day03 } from "./03/day03.js"
 import { day04 } from "./04/day04.js"
 import { day05 } from "./05/day05.js"
+import { day06 } from "./06/day06.js"
 
 console.clear()
 
@@ -14,7 +15,7 @@ type Day = {
   part2: () => void
 }
 
-const DAY_OBJECTS: Day[] = [day01, day02, day03, day04, day05]
+const DAY_OBJECTS: Day[] = [day01, day02, day03, day04, day05, day06]
 
 const dayObject = DAY_OBJECTS[day - 1]
 
