e7a1db15363e6103f2d49c69f8426aace359d3d3
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2023-10-30 20:21:57 +0000

    Complete day 5 part 2

--- src/05/day05.ts
+++ src/05/day05.ts
@@ -20,6 +20,41 @@ const part1 = () => {
   console.log(password)
 }
 
-const part2 = () => {}
+const part2 = () => {
+  let i = 0
+  const passwordChars: (string | null)[] = Array(8).fill(null)
+  console.log(passwordChars.map((c) => c || "_").join(""))
+
+  while (true) {
+    const hash = md5(`${DOOR_ID}${i}`)
+    i += 1
+
+    if (!hash.startsWith("00000")) {
+      continue
+    }
+    const pos = hash[5]!
+
+    if (pos < "0" || pos > "7") {
+      continue
+    }
+
+    const index = parseInt(pos)
+    if (passwordChars[index]) {
+      continue
+    }
+
+    const char = hash[6]!
+    passwordChars[index] = char
+
+    console.log(passwordChars.map((c) => c || "_").join(""))
+
+    if (passwordChars.every((c) => c !== null)) {
+      break
+    }
+  }
+
+  const password = passwordChars.join("")
+  console.log(password)
+}
 
 export const day05 = { part1, part2 }
